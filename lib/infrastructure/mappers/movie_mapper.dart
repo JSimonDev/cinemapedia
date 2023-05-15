db8fc57dedb8eb3ee45d3ce01f5f952d875b6c55
Jonathan Simon <yuditbertoli@gmail.com>
2023-05-15 04:57:36 -0400

    Infrastructure and Provider folder implemented

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/themoviedb/movie_themoviedb.dart';
+
+class MovieMapper {
+  static Movie theMovieDBToEntity(MovieTheMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '') 
+        ? 'https://image.tmdb.org/t/p/w500/${ moviedb.backdropPath }' 
+        : 'https://th.bing.com/th/id/R.c98045cd667447def428ad9b261c0ef4?rik=y8ioZ%2bsKto28oQ&pid=ImgRaw&r=0',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500/${ moviedb.posterPath }'
+        : 'no-poster',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+}
