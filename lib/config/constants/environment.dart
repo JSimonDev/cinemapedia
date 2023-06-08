3407101d80889876c98bfa05f5316e69dc255a15
Jonathan Simon <yuditbertoli@gmail.com>
2023-06-08 01:02:59 -0400

    two new constants added to .env document

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -1,8 +1,12 @@
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
+  static String theMovieDbKey =
+      dotenv.env["THE_MOVIEDB_KEY"] ?? "No hay API_KEY";
 
-  static String theMovieDbKey = dotenv.env["THE_MOVIEDB_KEY"] ?? "No hay API_KEY";
+  static String noActorPosterImage = dotenv.env["NO_ACTOR_POSTER_IMAGE"] ??
+      "No se encontro la variable NO_ACTOR_POSTER_IMAGE, revise el documento .env";
 
+  static String noMoviePosterImage = dotenv.env["NO_MOVIE_POSTER_IMAGE"] ??
+      "No se encontro la variable NO_MOVIE_POSTER_IMAGE, revise el documento .env";
 }
