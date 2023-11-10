9b8f582e711c1a2edb14c56cbfac79ace7a4d27e
Jonathan Simon <yuditbertoli@gmail.com>
2023-11-09 23:21:46 -0500

    Add movie masonry and poster link widgets

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -2,6 +2,8 @@ export 'movies/actor_details_widget.dart';
 export 'movies/movie_horizontal_listview.dart';
 export 'movies/movies_slideshow.dart';
 export 'movies/rating_and_date.dart';
+export 'movies/movie_masonry.dart';
+export 'movies/movie_poster_link.dart';
 
 export 'shared/custom_appbar.dart';
 export 'shared/custom_bottom_navbar.dart';
