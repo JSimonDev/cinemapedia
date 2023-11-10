753d2e697049113fbe8fd8a2e918f3573d57a650
Jonathan Simon <yuditbertoli@gmail.com>
2023-11-09 22:19:11 -0500

    Refactor main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-Future <void> main() async {
+Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    )
-  );
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
