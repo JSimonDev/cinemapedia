f344be305309ae5b14b547c36762555e1ee25a8b
Jonathan Simon <yuditbertoli@gmail.com>
2023-11-09 23:21:43 -0500

    Add movie masonry and infinite scrolling to favorites view

--- lib/presentation/views/home_views/favorites_view.dart
+++ lib/presentation/views/home_views/favorites_view.dart
@@ -1,13 +1,47 @@
+import 'package:cinemapedia/presentation/widgets/movies/movie_masonry.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+import '../../providers/providers.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  bool isLoading = false;
+  bool isLastPage = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadNextPage();
+  }
+
+  void loadNextPage() async {
+    if (isLoading || isLastPage) return;
+    isLoading = true;
+
+    final movies =
+        await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    isLoading = false;
+
+    if (movies.isEmpty) {
+      isLastPage = true;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
+      body: MovieMasonry(
+        movies: favoritesMovies,
+        loadNextPage: loadNextPage,
       ),
     );
   }
